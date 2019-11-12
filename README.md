49c1222c16c7cc8eef61aad0db50be92c6ccc820
LimerBoy <43149460+LimerBoy@users.noreply.github.com>
2019-11-13 01:01:54 +0300

    update.......

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# PS-Grabber
+# ADB-Hunter
 *Save all vulnerable device ips to file and connect with PhoneSploit*
 
 ___Installation on Linux:___
 ```
 Install python3 #sudo apt install python3 python3-pip
-git clone https://github.com/LimerBoy/PS-Grabber.git
+git clone https://github.com/LimerBoy/ADB-Hunter.git
 cd PS-Grabber/
 sudo pip3 install -r requirements.txt
 python3 grabber.py
@@ -15,7 +15,7 @@ python3 grabber.py
 ___Installation on Windows:___
 ```
 Install python3 #https://www.python.org/ftp/python/3.6.8/python-3.6.8-amd64.exe
-Download zip: https://github.com/LimerBoy/PS-Grabber/archive/master.zip
+Download zip: https://github.com/LimerBoy/ADB-Hunter.git
 Unzip folder PS-Grabber
 cd PS-Grabber/
 pip install -r requirements.txt
--- grabber.py
+++ grabber.py
@@ -2,19 +2,24 @@
 
 # Information
 print (r"""
-  ____    ____       ____                  _       _
- |  _ \  / ___|     / ___|  _ __    __ _  | |__   | |__     ___   _ __
- | |_) | \___ \    | |  _  | '__|  / _` | | '_ \  | '_ \   / _ \ | '__|
- |  __/   ___) |   | |_| | | |    | (_| | | |_) | | |_) | |  __/ | |
- |_|     |____/     \____| |_|     \__,_| |_.__/  |_.__/   \___| |_|
-                                                    Created by LimerBoy
+
+ █████╗ ██████╗ ██████╗       ██╗  ██╗██╗   ██╗███╗   ██╗████████╗███████╗██████╗ 
+██╔══██╗██╔══██╗██╔══██╗      ██║  ██║██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗
+███████║██║  ██║██████╔╝█████╗███████║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝
+██╔══██║██║  ██║██╔══██╗╚════╝██╔══██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗
+██║  ██║██████╔╝██████╔╝      ██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║
+╚═╝  ╚═╝╚═════╝ ╚═════╝       ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                                                Created by LimerBoy
 
    Save all vulnerable device ips to file and connect with PhoneSploit
 
-   * PS-Grabber : https://github.com/LimerBoy/PS-Grabber.git
-   * PhoneSploit: https://github.com/metachar/PhoneSploit.git
+   * PS-Grabber  > https://github.com/LimerBoy/ADB-Hunter.git
+   * PhoneSploit > https://github.com/metachar/PhoneSploit.git
 
 """)
+
+# If this shodan api key stops working just remove it from here and insert your key.
+api_key = 'PSKINdQe1GyxGgecYz2191H2JoS9qvgD'
 
 # Modules
 try:
@@ -26,7 +31,7 @@ except ImportError as error:
     module = str(error).split()[-1].replace('\'', '')
     print(
     '\nModule ' + module + ' is not installed!',
-    '\nsudo pip3 install -r requirements.txt'
+    '\npip3 install -r requirements.txt'
     )
     raise SystemExit
 
@@ -35,40 +40,40 @@ if not os.path.exists('logs'):
     os.mkdir('logs')
 
 # Settings
-pages_count = int(input('[?] How many pages need scan? : '))
-api = shodan.Shodan('PSKINdQe1GyxGgecYz2191H2JoS9qvgD') # If this api key stops working just remove it from here and insert yours
-date = datetime.datetime.today().strftime("%H.%M.%S-%d-%m")
+api          = shodan.Shodan(api_key) 
+date         = datetime.datetime.today().strftime("%H.%M.%S-%d-%m")
+product      = 'android debug bridge product:\"Android Debug Bridge\"'
+pages_start  = int(input('[?] Start scanning from page : '))
+pages_stop   = int(input('[?] Stop  scanning on   page : '))
 results_file = r'logs/ips-' + date + '.txt'
 
 # Load and save results from pages
-page = 1
 print('[?] If you press Ctrl + C scanning will be stopped!')
-while page < pages_count:
-    print('[' + str(page) + '/' + str(pages_count) + '] Loading page results... ')
+for page in range(pages_start, pages_stop, 1):
     try:
+        print('[' + str(page) + '/' + str(pages_stop) + '] Loading page results... ')
         time.sleep(1)
-        results = api.search('android debug bridge product:\"Android Debug Bridge\"', page = page)
+        results = api.search(product, page = page)
     except KeyboardInterrupt:
-        print('[!] Ctrl + C detected.. Stopping...')
+        print('\n[!] (Ctrl + C) detected.. Stopping...')
         break
     except shodan.exception.APIError as error:
-        print('[EXCEPTION] ' + str(error) + ' ' + 3 * '*')
+        print('[EXCEPTION] ' + str(error))
         time.sleep(3)
         continue
     else:
         for result in results['matches']:
             with open(results_file, 'a') as file:
                 file.write(result['ip_str'] + '\n')
-        page += 1
 
 # Display results after loading
-print('\n[+] Okay, all results from page ' + str(page) + ', was saved to ' + results_file)
-if input('[?] Show downloaded results? (y/n) : ').lower() in ['y', 'yes', 'true', '1', '+']:
-    results = 0
-    with open(results_file, 'r') as file:
-        for line in file.readlines():
-            print(line.replace('\n', ''))
-            results += 1
-    print('[+] Saved ' + str(results) + ' ips')
+if os.path.exists(results_file):
+	print('\n[+] Okay, all results from page ' + str(pages_start) + ' to page ' + str(pages_stop) +  ', was saved to ' + results_file)
+	if input('[?] Show downloaded results? (y/n) : ').lower() in ('y', 'yes', 'true', '1', '+'):
+	    with open(results_file, 'r') as file:
+	        lines = file.readlines()
+	        for line in lines:
+	            print(line.replace('\n', ''))
+	    print('[+] Saved ' + str(len(lines)) + ' ips')
 
 print('[$] Created by LimerBoy with Love.')
